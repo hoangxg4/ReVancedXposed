e156cdf5c01485042e0b1864ae31f576c77b41e3
ChsBuffer <33744752+chsbuffer@users.noreply.github.com>
2025-09-24 16:58:26 +0800

    Meta: Updated HideAds to improve the reliability.

--- app/src/main/java/io/github/chsbuffer/revancedxposed/meta/ads/Fingerprints.kt
+++ app/src/main/java/io/github/chsbuffer/revancedxposed/meta/ads/Fingerprints.kt
@@ -2,12 +2,10 @@ package io.github.chsbuffer.revancedxposed.meta.ads
 
 import io.github.chsbuffer.revancedxposed.findMethodDirect
 import io.github.chsbuffer.revancedxposed.strings
-import java.lang.reflect.Modifier
 
 val adInjectorFingerprint = findMethodDirect {
     findMethod {
         matcher {
-            modifiers = Modifier.PRIVATE
             returnType = "void"
             strings(
                 "SponsoredContentController.processValidatedContent",

4172e79af9c9ec8de3c42bb079f3d9997a7e59a5
ChsBuffer <33744752+chsbuffer@users.noreply.github.com>
2025-09-26 14:23:54 +0800

    YouTube: Fix `NavigationBarHook` on 20.39.xx

--- app/src/main/java/io/github/chsbuffer/revancedxposed/youtube/misc/navigation/NavigationBarHookPatch.kt
+++ app/src/main/java/io/github/chsbuffer/revancedxposed/youtube/misc/navigation/NavigationBarHookPatch.kt
@@ -103,7 +103,7 @@ fun YoutubeHook.NavigationBarHook() {
 
     // Fix YT bug of notification tab missing the filled icon.
     val tabActivityCairo = ::navigationEnumClass.clazz.enumValueOf("TAB_ACTIVITY_CAIRO")
-    if (tabActivityCairo != null && ytFillBellId != 0) {
+    if (tabActivityCairo != null && runCatching { ytFillBellId != 0 }.isSuccess) {
         ::setEnumMapFingerprint.dexMethodList.forEach { setEnumMapFingerprint ->
             fun processFields(obj: Any?, clazz: Class<*>) {
                 clazz.declaredFields.forEach { field ->

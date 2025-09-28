ca1490462c5126d655ed88bcf9dcb47247cdd968
ChsBuffer <33744752+chsbuffer@users.noreply.github.com>
2025-09-28 14:03:00 +0800

    Apply recycled bitmap workaround
    This commit applies a workaround for an issue where YouTube and YouTube Music may crash on certain ROMs due to recycled bitmaps in MediaSession::setMetadata.

--- app/src/main/java/io/github/chsbuffer/revancedxposed/shared/misc/CheckRecycleBitmapMediaSession.kt
+++ app/src/main/java/io/github/chsbuffer/revancedxposed/shared/misc/CheckRecycleBitmapMediaSession.kt
@@ -1,34 +1,33 @@
 package io.github.chsbuffer.revancedxposed.shared.misc
 
-import android.content.Intent
-import android.content.Intent.FLAG_ACTIVITY_NEW_TASK
-import android.net.Uri
-import app.revanced.extension.shared.Utils
+import android.media.MediaMetadata
+import android.media.session.MediaSession
+import android.os.Build
+import de.robv.android.xposed.XposedBridge
 import io.github.chsbuffer.revancedxposed.BaseHook
+import io.github.chsbuffer.revancedxposed.callStaticMethod
+import io.github.chsbuffer.revancedxposed.hookMethod
 import org.luckypray.dexkit.wrap.DexMethod
+import java.lang.reflect.Member
 
 fun BaseHook.CheckRecycleBitmapMediaSession() {
-    // Warning about YouTube and YouTube Music may crash on certain ROMs.
-
-//    Emulate the issue
-//    DexMethod("Landroid/media/MediaMetadata\$Builder;->build()Landroid/media/MediaMetadata;").hookMethod {
-//        after {
-//            val metadata = it.result as MediaMetadata
-//            metadata.getBitmap(MediaMetadata.METADATA_KEY_ART)?.recycle()
-//            metadata.getBitmap(MediaMetadata.METADATA_KEY_ALBUM_ART)?.recycle()
-//            metadata.getBitmap(MediaMetadata.METADATA_KEY_DISPLAY_ICON)?.recycle()
-//        }
-//    }
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) return
 
     if (runCatching { DexMethod("Landroid/media/MediaMetadata\$Builder;->calculateSampleSize(IIII)I").toMember() }.isSuccess) {
-//    if (true) {
-        val context = Utils.getContext()
-        val i = Intent(Intent.ACTION_VIEW)
-            .addFlags(FLAG_ACTIVITY_NEW_TASK)
-        i.data =
-            Uri.parse("https://github.com/chsbuffer/ReVancedXposed/issues/29#issuecomment-3084170279")
-        Utils.showToastLong("App Stability Issues Detected!")
-        context.startActivity(i)
-        Thread.sleep(30 * 1000)
+        XposedBridge.log("Applying MediaMetadata recycle Bitmap workaround (https://github.com/chsbuffer/ReVancedXposed/issues/29#issuecomment-3084170279)")
+
+        try {
+            MediaMetadata.Builder::class.java.getMethod("build").hookMethod {
+                before {
+                    (it.thisObject as MediaMetadata.Builder).setBitmapDimensionLimit(Integer.MAX_VALUE)
+                }
+            }
+
+            val setMetadata =
+                MediaSession::class.java.getMethod("setMetadata", MediaMetadata::class.java)
+            XposedBridge::class.java.callStaticMethod("deoptimizeMethod", setMetadata as Member)
+        } catch (err: Throwable) {
+            XposedBridge.log(err)
+        }
     }
 }

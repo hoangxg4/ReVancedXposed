2645d2cdfee907012bb50335ce9c2199623ffc26
ChsBuffer <33744752+chsbuffer@users.noreply.github.com>
2025-10-02 20:34:26 +0800

    bump 1.0.29

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
 
     defaultConfig {
         applicationId = "io.github.chsbuffer.revancedxposed"
-        versionCode = 28
+        versionCode = 29
         versionName = "1.0.$versionCode"
         val patchVersion = Properties().apply {
             rootProject.file("revanced-patches/gradle.properties").inputStream().use { load(it) }

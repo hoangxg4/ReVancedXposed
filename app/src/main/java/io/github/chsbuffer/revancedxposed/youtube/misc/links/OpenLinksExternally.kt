69f17c5cdc8f454a98f5c84ec17dec96fa7ea25d
hung319 <hungh6122@gmail.com>
2025-10-07 07:24:49 +0700

    update

--- app/src/main/java/io/github/chsbuffer/revancedxposed/youtube/misc/links/OpenLinksExternally.kt
+++ app/src/main/java/io/github/chsbuffer/revancedxposed/youtube/misc/links/OpenLinksExternally.kt
@@ -1,6 +1,5 @@
 package io.github.chsbuffer.revancedxposed.youtube.misc.links
 
-// Import chính xác đến file logic thực thi bằng Java
 import app.revanced.extension.youtube.patches.OpenLinksExternallyPatch
 import io.github.chsbuffer.revancedxposed.shared.misc.settings.preference.SwitchPreference
 import io.github.chsbuffer.revancedxposed.youtube.YoutubeHook
@@ -8,16 +7,17 @@ import io.github.chsbuffer.revancedxposed.youtube.misc.settings.PreferenceScreen
 import org.luckypray.dexkit.wrap.DexMethod
 
 fun YoutubeHook.openLinksExternallyHook() {
+    // Thêm công tắc vào menu. Giờ nó sẽ tự tìm thấy title/summary từ "kho chung".
     PreferenceScreen.MISC.addPreferences(
         SwitchPreference("revanced_external_browser")
     )
 
+    // Hook vào hàm tạo Intent
     DexMethod("Landroid/content/Intent;-><init>(Ljava/lang/String;)V").hookMethod {
         before { param ->
             val originalAction = param.args[0] as? String ?: return@before
             
-            // THAY ĐỔI DUY NHẤT Ở ĐÂY:
-            // Gọi đúng tên hàm gốc là "getIntent"
+            // Gọi đến hàm getIntent đã được sửa logic trong file Java
             param.args[0] = OpenLinksExternallyPatch.getIntent(originalAction)
         }
     }
